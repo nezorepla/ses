1241b4d565f6034402f9b4cbf55d8c427ee43a11
nezorepla <alperozen84@gmail.com>
2015-05-03 21:26:19 +0300

    create

---
+++ ses/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFreq = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFreq
+            // 
+            this.txtFreq.Location = new System.Drawing.Point(87, 27);
+            this.txtFreq.Name = "txtFreq";
+            this.txtFreq.Size = new System.Drawing.Size(100, 20);
+            this.txtFreq.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Freq:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Duration:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(87, 63);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 112);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFreq);
+            this.Name = "Form1";
+            this.Text = "ses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFreq;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ses/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+             FileStream stream = new FileStream("..\\"+txtName.Text.ToString()+".wav", FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(stream);
+            int RIFF = 0x46464952;
+            int WAVE = 0x45564157;
+            int formatChunkSize = 16;
+            int headerSize = 8;
+            int format = 0x20746D66;
+            short formatType = 1;
+            short tracks = 1;
+            int samplesPerSecond = 44100;
+            short bitsPerSample = 16;
+            short frameSize = (short)(tracks * ((bitsPerSample + 7) / 8));
+            int bytesPerSecond = samplesPerSecond * frameSize;
+            int waveSize = 4;
+            int data = 0x61746164;
+            int sn =int.Parse(txtTime.Text.ToString()); //60;
+            int samples = 88200 * 2*sn;
+            int dataChunkSize = samples * frameSize;
+            int fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
+            writer.Write(RIFF);
+            writer.Write(fileSize);
+            writer.Write(WAVE);
+            writer.Write(format);
+            writer.Write(formatChunkSize);
+            writer.Write(formatType);
+            writer.Write(tracks);
+            writer.Write(samplesPerSecond);
+            writer.Write(bytesPerSecond);
+            writer.Write(frameSize);
+            writer.Write(bitsPerSample);
+            writer.Write(data);
+            writer.Write(dataChunkSize);
+           double aNatural = 23000.0;
+            double ampl = 10000;
+            double perfect = 1.5;
+            double concert = 1.498307077;
+            double freq = double.Parse(txtFreq.Text.ToString());//aNatural * perfect;
+            for (int i = 0; i < samples / 4; i++)
+            {
+                double t = (double)i / (double)samplesPerSecond;
+                short s = (short)(ampl * (Math.Sin(t * freq * 2.0 * Math.PI)));
+                writer.Write(s);
+            }
+
+
+           //double  freq = aNatural * concert;
+           // for (int i = 0; i < samples / 4; i++)
+           // {
+           //     double t = (double)i / (double)samplesPerSecond;
+           //     short s = (short)(ampl * (Math.Sin(t * freq * 2.0 * Math.PI)));
+           //     writer.Write(s);
+           // }
+           // for (int i = 0; i < samples / 4; i++)
+           // {
+           //     double t = (double)i / (double)samplesPerSecond;
+           //     short s = (short)(ampl * (Math.Sin(t * freq * 2.0 * Math.PI) + Math.Sin(t * freq * perfect * 2.0 * Math.PI)));
+           //     writer.Write(s);
+           // }
+           // for (int i = 0; i < samples / 4; i++)
+           // {
+           //     double t = (double)i / (double)samplesPerSecond;
+           //     short s = (short)(ampl * (Math.Sin(t * freq * 2.0 * Math.PI) + Math.Sin(t * freq * concert * 2.0 * Math.PI)));
+           //     writer.Write(s);
+           // }
+            writer.Close();
+            stream.Close();
+            Application.Exit();
+        }
+
+        }
+    }
